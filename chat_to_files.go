aeca3e899fbf7c2f4c36b9760d505c3bc32a1124
Trent Millar <trent.millar@gmail.com>
2023-07-17 18:47:19 -0600

    generate files from markdown

--- chat_to_files.go
+++ chat_to_files.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"regexp"
-	"strings"
 )
 
 type fileItem struct {
@@ -11,14 +10,34 @@ type fileItem struct {
 }
 
 func parseChat(chat string) []fileItem {
-	regex := regexp.MustCompile("```(.*?)```")
+	regex := regexp.MustCompile("(\\S+)\n\\s*```[^\n]*\n([\\s\\S.]+?)```")
 	matches := regex.FindAllStringSubmatch(chat, -1)
 
 	files := make([]fileItem, 0, len(matches))
 
 	for _, match := range matches {
-		path := strings.Split(match[1], "\n")[0]
-		code := strings.Join(strings.Split(match[1], "\n")[1:], "\n")
+		path := match[1]
+
+		// Strip the filename of any non-allowed characters and convert / to \
+		regex = regexp.MustCompile(`[<>"|?*]`)
+		path = regex.ReplaceAllString(path, "")
+
+		// Remove leading and trailing brackets
+		regex = regexp.MustCompile(`^\[(.*)\]$`)
+		path = regex.ReplaceAllString(path, "\\1")
+
+		// Remove leading and trailing backticks
+		regex = regexp.MustCompile("^`(.*)`$")
+		path = regex.ReplaceAllString(path, "\\1")
+
+		// Remove trailing ]
+		regex = regexp.MustCompile("\\]$")
+		path = regex.ReplaceAllString(path, "")
+
+		// Get the code
+		code := match[2]
+
+		// Add the file to the list
 		files = append(files, fileItem{path, code})
 	}
 

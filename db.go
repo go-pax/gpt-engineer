a21d72207331096c1e302f04f9fda26f9702382d
Trent Millar <trent.millar@gmail.com>
2023-07-31 15:33:31 -0600

    adding database interface w/ File

--- db.go
+++ db.go
@@ -1,37 +1,47 @@
 package main
 
 import (
-	"github.com/geekr-dev/gpt-engineer/db/file"
-	"path/filepath"
+	"github.com/geekr-dev/gpt-engineer/database"
+	"github.com/geekr-dev/gpt-engineer/database/file"
 )
-
-type DB interface {
-	Set(key, val string) error
-	Get(key string) (string, error)
-	Path() string
-}
 
 type DBs struct {
 	canExecute bool
 	dbType     string
-	memory     DB
-	logs       DB
-	identity   DB
-	input      DB
-	workspace  DB
+	memory     database.Database
+	logs       database.Database
+	identity   database.Database
+	input      database.Database
+	workspace  database.Database
 }
 
-func NewFileDBs(rootPath string, projectPath string) DBs {
-	memoryPath := filepath.Join(projectPath, "memory")
-	workspacePath := filepath.Join(projectPath, "workspace")
-	identityPath := filepath.Join(rootPath, "identity")
+func NewDBs(projectPath string) (DBs, error) {
+	memoryDB, err := database.Open(projectPath, "memory")
+	if err != nil {
+		return DBs{}, err
+	}
+	logDB, err := database.Open(projectPath, "memory/logs")
+	if err != nil {
+		return DBs{}, err
+	}
+	workspaceDB, err := database.Open(projectPath, "workspace")
+	if err != nil {
+		return DBs{}, err
+	}
+	inputDB, err := database.Open(projectPath, "")
+	if err != nil {
+		return DBs{}, err
+	}
+	identityDB, err := database.Open("file://./", "identity")
+	if err != nil {
+		return DBs{}, err
+	}
 	return DBs{
 		canExecute: file.CanExecute,
-		dbType:     file.DbType,
-		memory:     file.New(memoryPath),
-		logs:       file.New(filepath.Join(memoryPath, "logs")),
-		identity:   file.New(identityPath),
-		input:      file.New(projectPath),
-		workspace:  file.New(workspacePath),
-	}
+		memory:     memoryDB,
+		logs:       logDB,
+		input:      inputDB,
+		workspace:  workspaceDB,
+		identity:   identityDB,
+	}, nil
 }
